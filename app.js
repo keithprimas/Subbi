4b949921ff12904b9c75fa22f2b50075abbbbd7b
Keith Primas <keithprimas@gmail.com>
2025-02-08 21:05:49 -0500

    setting up express server

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {PORT} from './config/env.js';
 
 const app = express();
 
@@ -6,8 +7,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Subbi API');
 });
 
-app.listen(3000, () => {
-  console.log('Subbi API is running on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Subbi API is running on http://localhost:${PORT}`);
 });
 
 export default app;
