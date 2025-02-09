1a63e30f9ecb2a492d34db2b0d4896c86ec9a4f5
Keith Primas <keithprimas@gmail.com>
2025-02-09 18:30:28 -0500

    connected to mongoDB

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import {PORT} from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
+import connectToDatabase from './DATABASE/mongodb.js';
 
 const app = express();
 
@@ -15,8 +16,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Subbi API');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Subbi API is running on http://localhost:${PORT}`);
+
+  await connectToDatabase();
 });
 
 export default app;
